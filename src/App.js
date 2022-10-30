c24519f559573d4c82804a77e6aa3e1731d7b25a
juujan786 <jhsurmovi123@gmail.com>
2022-10-30 18:32:28 +0500

    swiper carousal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "./styles.css";
+import { Pagination, FreeMode } from "swiper";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      Hello world!
-    </div>
+    <>
+    <h1>Carousal using SwiperJs</h1>
+      <Swiper
+      loop={true}
+      freeMode={true}
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="swiperSlide"><h1> Slide 1</h1></SwiperSlide>
+        <SwiperSlide className="swiperSlide"><h1> Slide 2</h1></SwiperSlide>
+        <SwiperSlide className="swiperSlide"><h1> Slide 3</h1></SwiperSlide>
+        <SwiperSlide className="swiperSlide"><h1> Slide 4</h1></SwiperSlide>
+        <SwiperSlide className="swiperSlide"><h1> Slide 5</h1></SwiperSlide>
+        <SwiperSlide className="swiperSlide"><h1> Slide 6</h1></SwiperSlide>
+        <SwiperSlide className="swiperSlide"><h1> Slide 7</h1></SwiperSlide>
+        <SwiperSlide className="swiperSlide"><h1> Slide 8</h1></SwiperSlide>
+        <SwiperSlide className="swiperSlide"><h1> Slide 9</h1></SwiperSlide>
+      </Swiper>
+    </>
   );
 }
-
-export default App;
